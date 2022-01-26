0376725d47a48bcb68327e2869a26279efd962d8
Chithara <chithara.2019160@iit.ac.lk>
2022-01-26 14:47:21 +0530

    declared database

--- BackendEnd_Development/Backend/Backend/Backend/settings.py
+++ BackendEnd_Development/Backend/Backend/Backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pocketguide',
+        'USER': 'pguide',
+        'PASSWORD': 'pguide'
     }
 }
 
