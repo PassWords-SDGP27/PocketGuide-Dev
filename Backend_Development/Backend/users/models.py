187651a24d686995fa7c28c7d72b3f69775580a0
Chithara <chithara.2019160@iit.ac.lk>
2022-01-27 06:20:16 +0530

    created a model user for database

--- Backend_Development/Backend/users/models.py
+++ Backend_Development/Backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# creating a database object named user
+class User(models.Model):
+    # defining attributes each model user has
+    name = models.CharField(max_length=200)
+
+    def __self__(self):
+        return self.name
