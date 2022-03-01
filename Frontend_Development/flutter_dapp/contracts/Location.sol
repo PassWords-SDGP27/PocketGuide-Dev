c61137ede6d73fcc81a1a926e49a6c63cdd5947e
Chithara <chithara.2019160@iit.ac.lk>
2022-03-01 21:59:44 +0530

    implemented add location method

--- Frontend_Development/flutter_dapp/contracts/Location.sol
+++ Frontend_Development/flutter_dapp/contracts/Location.sol
@@ -1,19 +1,37 @@
 pragma solidity ^0.4.0;
 
 contract Location {
-    function addLocation(string name){
+    event NewLocation(uint locationId, string name);
+
+    struct Location{
+        string locationName;
+    }
+
+    //array to store all locations
+    Location[] public locations;
+
+    //map to store which user added each location
+    mapping (uint => address) public locationToAdduser;
+
+    function addLocation(string memory _name) public{
+        uint id = locations.push(Location(_name)) - 1;
+        locationToAdduser[id] = msg.sender; //records which user added the location
+        emit NewLocation(id, _name);
+    }
+
+    function removeLocation(address _address) public{
+        locations.remove();
+    }
+
+    function addDescription() public{
 
     }
 
-    function removeLocation(uint256 _id){
+    function removeDescription() public{
 
     }
 
-    function addDescription(){
-
-    }
-
-    function removeDescription(){
+    function getLocation()public{
 
     }
 }
