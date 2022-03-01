67e87a759b4082172d6427b66d610e6912c74ef9
Chithara <chithara.2019160@iit.ac.lk>
2022-03-01 21:38:30 +0530

    added new functions

--- Frontend_Development/flutter_dapp/contracts/Location.sol
+++ Frontend_Development/flutter_dapp/contracts/Location.sol
@@ -1,7 +1,19 @@
 pragma solidity ^0.4.0;
 
 contract Location {
-    function Location(){
+    function addLocation(string name){
+
+    }
+
+    function removeLocation(uint256 _id){
+
+    }
+
+    function addDescription(){
+
+    }
+
+    function removeDescription(){
 
     }
 }
