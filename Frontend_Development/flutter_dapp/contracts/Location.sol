58716c2486e09a7b95a60013a16bddbc351f8539
Chithara <chithara.2019160@iit.ac.lk>
2022-03-08 12:00:12 +0530

    deleted existing implementation

--- Frontend_Development/flutter_dapp/contracts/Location.sol
+++ Frontend_Development/flutter_dapp/contracts/Location.sol
@@ -1,56 +1,7 @@
 // SPDX-License-Identifier:MIT
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.7;
 
 contract Location {
-    event NewLocation(uint locationId, string name);
-
-    //string locationName;
-
-    struct Location{
-        string locationName;
-    }
-
-    //array to store all locations
-    Location[] public locations;
-
-    //map to store which user added each location
-    mapping (uint => address) public locationToAdduser;
-    mapping (address => Location[]) locationsNew;
-
-    function addLocation(string memory _name) public{
-//        uint id = locations.push(Location(_name)) - 1;
-        locationsNew[msg.sender].push(Location({locationName: _name}));
-        //emit NewLocation(_name);
-    }
-
-    function findLocation (string _locationName) public returns (uint){
-        for (uint i=0; i<locations.length; i++){
-            if(locations[i].locationName == _locationName){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    function deleteLocation (string memory _locName) external {
-        uint locId = findLocation(_locName);
-
-        if (locId != -1){
-            delete locations[locId];
-        }
-    }
-
-
-    function addDescription() public{
-
-    }
-
-    function editDescription() public{
-
-    }
-
-    function getLocation()public{
-
-    }
+    
 }
