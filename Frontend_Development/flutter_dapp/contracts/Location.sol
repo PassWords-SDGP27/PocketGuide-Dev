24c004234915f7f3d2ffe7faf6b18f1d5f0f2d18
Chithara <chithara.2019160@iit.ac.lk>
2022-03-08 12:11:17 +0530

    implemented find location function

--- Frontend_Development/flutter_dapp/contracts/Location.sol
+++ Frontend_Development/flutter_dapp/contracts/Location.sol
@@ -16,4 +16,15 @@ contract Location {
     function addLocation(string memory _locationName, string memory _locationDesc) external {
         locations.push(LocationStruct(_locationName, _locationDesc)); //add new location to the locations array
     }
+
+    //Function returns array index of location if location exists, else returns size of array
+    function findLocation (string memory _locationName) public view returns (uint){
+        for (uint i=0; i<locations.length; i++){
+            //check if current location name is equal to find location name
+            if(keccak256(abi.encodePacked((locations[i].locationName))) == keccak256(abi.encodePacked((_locationName)))){ //comparing two strings
+                return i;//then return array index of location 
+            }
+        }
+        return locations.length;//return size of array if location name NOT FOUND
+    }
 }
