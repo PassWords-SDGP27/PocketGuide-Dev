89844cf212e877753f7ba22ec4c63adb7817f1a4
Chithara <chithara.2019160@iit.ac.lk>
2022-03-08 12:03:19 +0530

    implemented add location function

--- Frontend_Development/flutter_dapp/contracts/Location.sol
+++ Frontend_Development/flutter_dapp/contracts/Location.sol
@@ -5,8 +5,15 @@ pragma solidity ^0.8.7;
 contract Location {
 
     //Struct for Location
-    struct LocationStruct{
+    struct LocationStruct {
         string locationName;
         string locationDesc;
+    }
+
+    LocationStruct[] public locations;//array to store locations
+
+    //Add new location function
+    function addLocation(string memory _locationName, string memory _locationDesc) external {
+        locations.push(LocationStruct(_locationName, _locationDesc)); //add new location to the locations array
     }
 }
