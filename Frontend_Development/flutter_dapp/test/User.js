127fa6aa4de56c42920c349f377dd2483b602d83
Chithara <chithara.2019160@iit.ac.lk>
2022-03-22 14:50:25 +0530

    test

--- Frontend_Development/flutter_dapp/test/User.js
+++ Frontend_Development/flutter_dapp/test/User.js
@@ -3,7 +3,7 @@ const User = artifacts.require("User") ;
 contract("User" , () => {
     it("User Testing" , async () => {
        const user = await User.deployed() ;
-       await user.createUser("testuser@gmail.com", "testuser", "testPassword", "0777777777") ;
+       await user.createUser("testuser@gmail.com", "testuser", "testPassword", "0777777777");
        const result = await user.username() ;
        assert(result === "chithzz") ;
     });
