b5f3129bc51a7c342aab6c797ff885cf6ff81fd0
Wiras Fernando <86821085+wiras26@users.noreply.github.com>
2022-03-24 16:18:38 +0530

    add_location

--- Frontend_Development/ios/Flutter/flutter_export_environment.sh
+++ Frontend_Development/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Flutter\flutter_maps-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\flutter_form_validation_personalInformation-master\flutter_form_validation_personalInformation-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
