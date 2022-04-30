3dcb1b15b0431835737a92dc87349ea4914ffdec
Wiras Fernando <86821085+wiras26@users.noreply.github.com>
2022-04-30 12:19:28 +0530

    Added search location navigator

--- Frontend_Development/flutter_dapp/lib/pages/homePage.dart
+++ Frontend_Development/flutter_dapp/lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter_dapp/pages/homeSubPages/SearchLocation.dart';
 import 'package:flutter_dapp/pages/homeSubPages/feedbackPage.dart';
 import 'package:flutter_dapp/pages/homeSubPages/locationSelectorPage.dart';
 import 'package:flutter_dapp/pages/homeSubPages/LandingPage.dart';
@@ -15,7 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-  final List _children = [LandingPage(), ARCoreController(), LocationSelectorPage(), FeedbackPage() ];
+  final List _children = [LandingPage(), SearchLocation(),ARCoreController(), LocationSelectorPage(), FeedbackPage() ];
   void _onItemTapped(int index) {
     setState(() {
       _currentIndex  = index;
@@ -38,6 +39,10 @@ class _HomePageState extends State<HomePage> {
         BottomNavigationBarItem(
           icon: new Icon(Icons.home),
           label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.search),
+          label: 'Search',
         ),
          BottomNavigationBarItem(
           icon: new Icon(Icons.view_in_ar),
